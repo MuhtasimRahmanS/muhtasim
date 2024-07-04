ea3fde0210d819ea9da6a3ca472a7bf437e9d129
MuhtasimRahmanS <muhtasimrahman01@gmail.com>
2024-07-04 19:45:08 +0600

    banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -5,20 +5,18 @@ import Image from "next/image";
 
 const Banner = () => {
   return (
-    <div id="about">
+    <div>
       <div className="hero  h-[70vh]">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <Image alt="image" src={photo} className="max-w-sm rounded-lg  " />
+        <div className="hero-content flex-col justify-between w-full lg:flex-row-reverse">
+          <div>
+            <Image alt="image" src={photo} className="max-w-sm rounded-lg  " />
+          </div>
           <div>
             <h1 className="text-4xl font-bold">
               Hello I&apos;m <br /> Muhtasim Rahman
             </h1>
             <h1 className="text-3xl font-bold">React Web Developer</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
+            <p className="py-6">{/* todo */}</p>
             <a className="btn btn-primary" href="/resume.pdf" download>
               <button>Download Resume</button>
             </a>
